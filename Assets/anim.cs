7908852ac31ad7137cad2fec6fe6567887c3df75
Vladislav <ternovoyvladislav@mail.ru>
2013-11-18 21:10:09 +0200

    assets

---
+++ Assets/anim.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class anim : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	animation.Play("Take 0010");
+		animation.wrapMode = WrapMode.Loop;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
